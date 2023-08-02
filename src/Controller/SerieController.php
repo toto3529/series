6d3cc9f0a62db4449a3366e86bb3e1d1da1bd00b
Antoine MARTIN <antoine.martin2023@campus-eni.fr>
2023-08-02 17:27:39 +0200

    mi-module 5

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -45,6 +47,46 @@ class SerieController extends AbstractController
      */
     public function create(): Response
     {
+
+        return $this->render('serie/create.html.twig');
+
+}
+
+ /**
+     * @Route("/series/demo", name="serie_em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //créé une instance de mon entitée
+        $serie = new Serie();
+
+        //hydrate toutes les propriétés
+        $serie->setName('pif paf pouf');
+        $serie->setBackdrop('ersdfsd');
+        $serie->setPoster('dsfsdf');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate(new \DateTime("- 6 month"));
+        $serie->setGenres('drama');
+        $serie->setOverview('bla bla bla');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('Canceled');
+        $serie->setTmdbId(321568);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        //$entityManager->remove($serie);
+
+        $serie->setGenres('comedy');
+
+        $entityManager->flush();
+
 
         return $this->render('serie/create.html.twig');
 
