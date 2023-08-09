a31d35bc3fc7f02fe39f86fd6b3ca965de30a4d0
Antoine MARTIN <antoine.martin2023@campus-eni.fr>
2023-08-09 16:10:22 +0200

    fin module 8

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,12 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+     * @Route("/admin", name="admin_")
+     */
+    
+
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin/dashboard", name="admin_dashboard")
+     * @Route("/dashboard", name="dashboard")
      */
-    
+
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
